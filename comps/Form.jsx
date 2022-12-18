1c690f79747edfdfef420fc9b04eddfeac3392fc
Andrew Bustos <infernious@live.com>
2022-12-18 01:45:07 -0800

    Initial site
    Details dropdown still needs to be added for Home

---
+++ comps/Form.jsx
@@ -0,0 +1,157 @@
+import emailjs from "@emailjs/browser"
+import { useRef, useState } from "react"
+
+export const Form = ({ contact_styles }) => {
+	const form = useRef()
+	const [sent, setSent] = useState(false)
+
+	const sendEmail = (e) => {
+		e.preventDefault() // prevents the page from reloading when you hit “Send”
+		emailjs
+			.sendForm(
+				"service_hlt6tg3",
+				"template_ukw3d0m",
+				form.current,
+				"vz9_xJj9gXLm2XWBt"
+			)
+			.then(
+				() => {
+					setSent(true)
+				},
+				() => {}
+			)
+	}
+
+	return (
+		(sent && <h3 style={{}}>Your message has been sent!</h3>) || (
+			<div className={contact_styles.form} onSubmit={sendEmail}>
+				<form ref={form}>
+					<h3>Name</h3>
+					<input
+						type='text'
+						id='fname'
+						name='fname'
+						placeholder='First'
+						required
+						enterKeyHint='next'
+						className={contact_styles.textfield}
+						style={{ marginRight: 10 }}
+					/>
+					<input
+						type='text'
+						id={contact_styles.lname}
+						name='lname'
+						placeholder='Last'
+						required
+						enterKeyHint='next'
+						className={contact_styles.textfield}
+					/>
+					<br />
+					<h3>Phone</h3>
+					<input
+						type='tel'
+						id='phone'
+						name='phone'
+						required
+						enterKeyHint='next'
+						className={contact_styles.textfield}
+						pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+						placeholder='xxx-xxx-xxxx'
+						onInvalid={(e) =>
+							e.target.setCustomValidity(
+								"Please enter phone number in the correct format: xxx-xxx-xxxx"
+							)
+						}
+					/>
+					<br />
+					<h3>Email</h3>
+					<input
+						type='email'
+						id='email'
+						name='email'
+						required
+						enterKeyHint='next'
+						className={contact_styles.textfield}
+					/>
+					<br />
+					<h3>Address</h3>
+					<input
+						type='text'
+						id='street-address'
+						name='street-address'
+						autoComplete='street-address'
+						required
+						enterKeyHint='next'
+						className={contact_styles.textfield}
+						placeholder='Street address'
+					/>
+					<br />
+					<br />
+					<input
+						type='text'
+						id='address-line2'
+						name='address-line2'
+						autoComplete='address-line2'
+						required
+						enterKeyHint='next'
+						placeholder='Address line 2'
+						className={contact_styles.textfield}
+					/>
+					<br />
+					<br />
+					<input
+						type='number'
+						id='postal-code'
+						name='postal-code'
+						autoComplete='postal-code'
+						enterKeyHint='next'
+						placeholder='Zip/postal code'
+						className={contact_styles.textfield}
+					/>
+					<br />
+					<br />
+					<input
+						type='text'
+						id='city'
+						name='city'
+						autoComplete='address-level2'
+						enterKeyHint='next'
+						placeholder='City'
+						className={contact_styles.textfield}
+					/>
+					<br />
+					<br />
+					<input
+						type='text'
+						id='state'
+						name='state'
+						autoComplete='address-level1'
+						enterKeyHint='next'
+						placeholder='State'
+						className={contact_styles.textfield}
+					/>
+					<br />
+					<h3>Describe Services Needed</h3>
+					<textarea className={contact_styles.desc} name='desc' rows='10' />
+					<br />
+					<hr
+						style={{
+							border: "none",
+							height: 1,
+							backgroundColor: "black",
+						}}
+					/>
+					<p style={{ fontSize: 12 }}>
+						The protection of your privacy is important to us at Benzo Painting.
+						Rest assured that the personal information you provide us with will
+						not be disclosed to other parties and will be used solely for the
+						purpose of bringing you the best service possible.
+					</p>
+					<button className='button' type='submit' value='Send'>
+						Submit
+					</button>
+				</form>
+			</div>
+		)
+	)
+}
---
+++ pages/contact.js
@@ -0,0 +1,127 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+import contact_styles from "../styles/Contact.module.css"
+import { Form } from "../comps/Form"
+import Link from "next/link"
+
+const Contact = () => {
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Benzo Painting</title>
+				<meta name='description' content='Contact Benzo Painting Services' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<style jsx>{`
+				@media (max-width: 600px) {
+					#fname {
+						margin-bottom: 10px;
+					}
+				}
+			`}</style>
+			<nav className={styles.nav}>
+				<Link href='/'>Home</Link>
+				{/* <Link href='/about'>About</Link>
+				<Link href='/services'>Services</Link>
+				<Link href='/reviews'>Testimonies</Link> */}
+				<Link href='/contact'>Contact Us</Link>
+				<Link href='/contact' className='button' id={styles["nav-btn"]}>
+					Get A Quote
+				</Link>
+			</nav>
+			<main className={styles.main}>
+				<div className={contact_styles.content}>
+					<div className={contact_styles.form_area}>
+						<h2>Send Us a Message</h2>
+						<Form contact_styles={contact_styles} />
+					</div>
+					<div className={contact_styles.contact_area}>
+						<div
+							style={{
+								backgroundColor: "white",
+								textAlign: "center",
+								width: "100%",
+								boxShadow: "2px 5px 10px gray",
+								borderRadius: 10,
+							}}
+						>
+							<h3>Contact Us Now</h3>
+							<p>benzopainting@gmail.com</p>
+							<h3>(209) 403 7980</h3>
+						</div>
+						<div></div>
+					</div>
+				</div>
+				<div className={styles.contact}>
+					<div className={styles.center_content}>
+						<h3>Contact Us</h3>
+						<p>
+							For more help please contact us at (209) 403 7980. For other
+							questions or projects that you need help with, please email us
+							with the information at benzopainting@gmail.com.
+						</p>
+						<p>A representative will respond in less than 24 hours.</p>
+					</div>
+				</div>
+			</main>
+			<footer className={styles.footer}>
+				<div className={styles.center_content}>
+					<div
+						style={{
+							alignSelf: "flex-start",
+						}}
+					>
+						<div className={styles.contacts}>
+							<Image
+								src='/logo.png'
+								width={133}
+								height={130}
+								style={{ borderRadius: 15 }}
+								alt='logo'
+							/>
+							<div style={{ marginLeft: "1rem" }}>
+								<h3>Contact</h3>
+								<p style={{ lineHeight: 2 }}>
+									(209) 403 7980
+									<br />
+									benzopainting@gmail.com
+								</p>
+							</div>
+						</div>
+						<div>
+							<Image
+								src='/livingroom.png'
+								width={409}
+								height={223}
+								style={{ borderRadius: 15, width: "100%", marginTop: "12%" }}
+								alt='living room'
+							/>
+						</div>
+					</div>
+					<div style={{ alignSelf: "flex-start", marginLeft: "auto" }}>
+						<h3>Service Areas</h3>
+						<p style={{ lineHeight: 2 }}>
+							Stockton, CA | Manteca, CA
+							<br />
+							San Ramon, CA | Walnut Creek, CA
+							<br /> Lathrop, CA | Ripon, CA
+							<br />
+							Lafayette, CA | Pleasanton, CA
+							<br /> Livermore, CA | Turlok, CA
+							<br /> Lodi, CA | Salida, CA
+							<br /> Tracy, CA | Galt, CA
+							<br /> Concord, CA | Modesto, CA
+							<br /> Discovery Bay, CA | Oakdale, CA
+							<br /> Sunol, CA | Hayward, CA
+							<br /> Dublin, CA | Escalon, CA
+							<br /> Orinda, CA
+						</p>
+					</div>
+				</div>
+			</footer>
+		</div>
+	)
+}
+
+export default Contact
