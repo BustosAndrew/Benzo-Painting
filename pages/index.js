9127967e173438799602e6b2e1df98945705a29c
Andrew Bustos <infernious@live.com>
2023-03-12 20:50:25 -0700

    Added services keyword

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 				<meta name='description' content='Benzo Painting Services' />
 				<meta
 					name='keywords'
-					content='painting, drywall, interior, exterior'
+					content='painting, drywall, interior, exterior, services'
 				></meta>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
